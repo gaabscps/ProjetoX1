cd47e4afe21700ae3c777d1797661ffad06145b4
gaabscps <gaabscps@gmail.com>
2023-06-02 21:06:39 -0300

    📱 Adaptative design

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -11,10 +11,38 @@ import { Body } from "@/components/Body";
 import HowToPlay from "./components/HowToPlay";
 import LandingPageFaq from "./components/FAQ";
 import { Footer } from "@/components/Footer";
+import { useEffect } from "react";
 
 interface PageProps {}
 
 export default function Landing({}: PageProps) {
+  function updateScale() {
+    if (typeof window !== "undefined") {
+      let screenWidth = window.innerWidth;
+      let scale = screenWidth / 1110;
+
+      if (screenWidth >= 1110) {
+        scale = 1;
+      }
+
+      document.body.style.transform = "scale(" + scale + ")";
+    }
+  }
+
+  useEffect(() => {
+    updateScale(); // Chama a função imediatamente para aplicar a escala inicial
+    if (typeof window !== "undefined") {
+      updateScale(); // Chama a função imediatamente para aplicar a escala inicial
+
+      window.addEventListener("load", updateScale);
+      window.addEventListener("resize", updateScale);
+
+      return () => {
+        window.removeEventListener("load", updateScale);
+        window.removeEventListener("resize", updateScale);
+      };
+    }
+  }, []);
   return (
     <>
       <Header />
