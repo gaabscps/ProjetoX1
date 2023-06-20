4fe576c7af7ac45d013a8be6b5e92aab5a7774ea
gaabscps <gaabscps@gmail.com>
2023-06-20 19:04:07 -0300

    🚑 Fix NextJs Window validation

--- src/app/landing/components/StatsSection/index.tsx
+++ src/app/landing/components/StatsSection/index.tsx
@@ -4,46 +4,55 @@ import { useEffect, useRef, useState } from "react";
 export function StatsSection() {
   const textRef = useRef<HTMLParagraphElement>(null);
   const textRef2 = useRef<HTMLParagraphElement>(null);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener("resize", handleResize);
+    if (typeof window !== "undefined") {
+      setWindowWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+    }
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
     };
   }, []);
 
   useEffect(() => {
     const duration = Math.max(2000 * (windowWidth / 1920), 2000); // Adjust the divisor (1920) as needed
 
-    const animation = textRef?.current?.animate(
-      [{ transform: "translateX(0%)" }, { transform: `translateX(-230px)` }],
-      {
-        duration,
-        iterations: Infinity,
-        easing: "linear",
-      }
-    );
+    const animation =
+      textRef?.current?.animate &&
+      textRef.current.animate(
+        [{ transform: "translateX(0%)" }, { transform: `translateX(-230px)` }],
+        {
+          duration,
+          iterations: Infinity,
+          easing: "linear",
+        }
+      );
 
     return () => animation?.cancel();
   }, [windowWidth]);
 
   useEffect(() => {
-    const duration = Math.max(2000 * (windowWidth / 1920), 2000); // Adjust the divisor (1920) as needed
+    const duration = Math.max(2000 * (windowWidth / 1920), 2000);
 
-    const animation = textRef2?.current?.animate(
-      [{ transform: `translateX(-230px)` }, { transform: "translateX(0%)" }],
-      {
-        duration,
-        iterations: Infinity,
-        easing: "linear",
-      }
-    );
+    const animation =
+      textRef2?.current?.animate &&
+      textRef2.current.animate(
+        [{ transform: `translateX(-230px)` }, { transform: "translateX(0%)" }],
+        {
+          duration,
+          iterations: Infinity,
+          easing: "linear",
+        }
+      );
 
     return () => animation?.cancel();
   }, [windowWidth]);
