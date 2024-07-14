582d32c7cca1437709df6474daaa67e43db3f313
gabrielandrade-bnex <gaabscps@gmail.com>
2024-07-14 06:25:39 -0300

    🏷️ Add gamelist type

--- src/app/landing/components/GamesSection/index.tsx
+++ src/app/landing/components/GamesSection/index.tsx
@@ -1,34 +1,32 @@
-import { Body } from '@/components/Body';
-import { Carrossel } from '@/components/Carousel';
-import { Element } from 'react-scroll';
-import { GamesList } from '@/types/GamesList';
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import { Body } from '@/components/Body'
+import { Carrossel } from '@/components/Carousel'
+import { Element } from 'react-scroll'
+import { GamesList } from '@/types/GamesList'
 
 interface GamesSectionProps {
-  games: GamesList[];
+  games: GamesList[]
 }
 
 export function GamesSection({ games }: GamesSectionProps) {
-
-  const availableGames = games.map((item) => (
+  const availableGames = games.map((item: GamesList) => (
     <div
-      className="h-100 d-flex justify-content-center align-items-center games-carousel"
+      className='h-100 d-flex justify-content-center align-items-center games-carousel'
       style={{ width: '231px', height: '126px' }}
       key={1}
     >
       <img src={item.thumbnail} />
     </div>
-  ));
+  ))
 
-  return (
-    games && games.length > 0 ?
-      <>
-        <Element name="games">
-          <Body carousel marginBottom="130px" marginTop='130px'>
-            <Carrossel isBanner title="Jogos disponíveis" items={availableGames} />
-          </Body>
-        </Element>
-      </>
-      :
-      null
-  );
+  return games && games.length > 0 ? (
+    <>
+      <Element name='games'>
+        <Body carousel marginBottom='130px' marginTop='130px'>
+          <Carrossel isBanner title='Jogos disponíveis' items={availableGames} />
+        </Body>
+      </Element>
+    </>
+  ) : null
 }
